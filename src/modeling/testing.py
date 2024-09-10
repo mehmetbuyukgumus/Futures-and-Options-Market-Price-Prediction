a0336f153a7963dd584a3249fd19a40f0683719a
mehmetbuyukgumus <mehmetbgumus@gmail.com>
2024-09-10 21:17:14 +0300

    Todays predictions

--- src/modeling/testing.py
+++ src/modeling/testing.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 
 def prep_data_of_model():
-    data = historical_prices("PGSUS.IS", "THYAO.IS", "2020-07-08", "2024-09-09")
+    data = historical_prices("KOZAL.IS", "KOZAA.IS", "2020-07-08", "2024-09-10")
     data.dropna(inplace=True)
     return data
 
